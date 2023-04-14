eee6d3ec3f538276b90fff424557f8ef693c0250
Viktors Garkavijs <v@mixefy.jp>
2023-04-14 12:18:15 +0900

    :test_tube: Fix failing test

--- src/test/kotlin/jp/mixefy/poc/spring/envs/GammaEnvTests.kt
+++ src/test/kotlin/jp/mixefy/poc/spring/envs/GammaEnvTests.kt
@@ -15,10 +15,10 @@ class GammaEnvTests(
     @Test
     fun `environment setting propagation works correctly`() {
         apiController.envName() shouldBe "env.name: gamma"
-        apiController.envNetwork() shouldBe "env.network: dev"
+        apiController.envNetwork() shouldBe "env.network: prod"
         apiController.pocDbInstance() shouldBe "poc.db-instance: gamma-instance"
         apiController.pocMyServiceAddress() shouldBe
-                "poc.my-service.address: my-service.gamma.gw.dev-poc-main-k8s01.cluster.gamma.mixefy.local:9090"
+                "poc.my-service.address: my-service.gamma.gw.prod-poc-main-k8s01.cluster.gamma.mixefy.local:9090"
     }
 
 }
